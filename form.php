63e3c00c4da423915610a137108c9850e81f3cbe
snekhasri <62427334+snekhasri@users.noreply.github.com>
2020-06-26 16:48:01 +0530

    echo.php

---
+++ form.php
@@ -0,0 +1,18 @@
+<?php
+echo "<h2>Your Input:</h2>";
+echo $name;
+echo "<br>";
+echo $dob;
+echo "<br>";
+echo $add;
+echo "<br>";
+echo $email;
+echo "<br>";
+echo $phone;
+echo "<br>";
+echo $website;
+echo "<br>";
+echo $comment;
+echo "<br>";
+echo $gender;
+?>
